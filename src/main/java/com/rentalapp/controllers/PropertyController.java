57231b2257d911679a4c67f34216c6d74bb7fe8a
Sasha <laplata228@gmail.com>
2025-01-15 13:40:11 +0200

    Добавив ввід тегів

--- src/main/java/com/rentalapp/controllers/PropertyController.java
+++ src/main/java/com/rentalapp/controllers/PropertyController.java
@@ -1,7 +1,9 @@
 package com.rentalapp.controllers;
 
 import com.rentalapp.models.Property;
+import com.rentalapp.models.Tag;
 import com.rentalapp.services.PropertyService;
+import com.rentalapp.services.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,14 +25,18 @@ import java.util.List;
 public class PropertyController {
     private final PropertyService propertyService;
 
+    private final TagService tagService;
     @GetMapping("/add")
     public String getNewProperty(Model model){
         model.addAttribute("property", new Property());
+        model.addAttribute("availableTags", tagService.getAllTags()); // Додаємо всі доступні теги
         return "property/addProperty";
     }
     @PostMapping("/add")
-    public String addProperty(@ModelAttribute("property") Property property){
-        propertyService.addProperty(property);
+    public String addProperty(@ModelAttribute("property") Property property,
+                              @RequestParam(name = "selectedTags", required = false) List<Long> selectedTagIds){
+
+        propertyService.addProperty(property,  selectedTagIds);
         return "redirect:/home";
     }
 
